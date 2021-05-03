261e99d5e4c1fdca322173bd95883fee71177293
Berkan Komurcu <berkankomurcu@gmail.com>
2021-05-03 16:43:20 +0300

    Add project files.

---
+++ WebApplication5/Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models.Entity;
+
+namespace WebApplication5.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p1)
+        {
+            db.TBLSATISLAR.Add(p1);
+            db.SaveChanges();
+            return View("Index");
+        }
+        
+    }
+}
---
+++ WebApplication5/Controllers/UrunController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models.Entity;
+
+namespace WebApplication5.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urn = db.TBLURUNLER.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir",urn);
+        }
+        public ActionResult Guncelle(TBLURUNLER p1)
+        {
+            var urn = db.TBLURUNLER.Find(p1.URUNID);
+            urn.URUNAD = p1.URUNAD;
+            urn.MARKA = p1.MARKA;
+            urn.STOK = p1.MARKA;
+            urn.FİYAT = p1.FİYAT;
+            //urn.URUNKATEGORI = p1.URUNKATEGORİ;
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            urn.URUNKATEGORİ = ktg.KATEGORIID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+    }
+}
